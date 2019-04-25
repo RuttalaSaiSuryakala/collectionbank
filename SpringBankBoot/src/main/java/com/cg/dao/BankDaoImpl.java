5fee990413425c731ecda1310c81ae312899c859
RuttalaSaiSuryakala <48008674+RuttalaSaiSuryakala@users.noreply.github.com>
2019-04-25 18:14:27 +0530

    Add files via upload

---
+++ SpringBankBoot/src/main/java/com/cg/dao/BankDaoImpl.java
@@ -0,0 +1,38 @@
+package com.cg.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import com.cg.model.CustomerDetails;
+
+@Repository
+@Component("bankDao")
+public class BankDaoImpl implements BankDao {
+
+	@PersistenceContext
+	EntityManager entityManager;
+	public CustomerDetails register(CustomerDetails cd) {
+		
+	entityManager.persist(cd);
+
+		return cd;
+	}
+	public int login(CustomerDetails c) {
+		// TODO Auto-generated method stub
+		int accountNo = 0;
+		
+		CustomerDetails cd = entityManager.find(CustomerDetails.class, c.getAccountNo());
+		if(cd.getPassword().equals(c.getPassword())) {
+			accountNo = c.getAccountNo(); 
+		}
+		return accountNo;
+	}
+
+	
+
+}
